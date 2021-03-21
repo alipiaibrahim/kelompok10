dcb0c8d42e46ae301a7a7d3397aeba96bb7c5a5e
alipiaibrahim <ibrahimalipia56@gmail.com>
2021-03-21 23:35:01 +0700

    menambahkan js scroll dan css

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-<script type="text/javascript" src="scroll.js"></script>
-<script type="text/javascript" src="ScrollTrigger.min.js"></script>
+<script type="text/javascript" src="js-main/scroll.js"></script>
+<script type="text/javascript" src="js-main/ScrollTrigger.min.js"></script>
 </body>
 </html>
